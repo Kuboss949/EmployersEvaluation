63b5acfc996ec9926a28de670b5e31bede67e6f0
Kuboss949 <jakub.lopataa01@gmail.com>
2025-04-24 11:12:52 +0200

    ManagerAnswerText class

---
+++ OcenaPracowniczaLys/Data/ManagerAnswersText.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OcenaPracowniczaLys.Data;
+
+public partial class ManagerAnswersText
+{
+    public int ManagerAnswerTextId { get; set; }
+
+    public int ManagerAnswerId { get; set; }
+
+    public int QuestionId { get; set; }
+
+    public string? AnswerText { get; set; }
+
+    public virtual ManagerAnswer ManagerAnswer { get; set; } = null!;
+
+    public virtual Question Question { get; set; } = null!;
+}
